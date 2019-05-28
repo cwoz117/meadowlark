4deda0994cc0599143990113129f115e8f11d601
Christopher Wozniak <cwozniak@aboutstaffing.com>
2019-05-28 13:52:39 -0600

    updated with static content

--- routing.js
+++ routing.js
@@ -1,19 +1,32 @@
 const http = require('http');
+const fs = require('fs');
+
+const serveStatic = (res, path, contentType, responseCode) => {
+  if (!responseCode) responseCode = 200;
+  fs.readFile(__dirname + path, (err, data) => {
+    if(err) {
+      res.writeHead(500, {'Content-Type': 'text/plain'});
+      res.end('500 - internal Error');
+    } else {
+      res.writeHead(responseCode, {'Content-Type': contentType});
+      res.end(data);
+    }
+  });
+}
 
 http.createServer( (req, res) => {
   const path = req.url.replace(/\/?(?:\?.*)?$/, '').toLowerCase();
   switch(path){
     case '':
-      res.writeHead(200, {'content-type':'text/plain' });
-      res.end('Homepage');
+      serveStatic(res, '/static/home.html', 'text/plain');
       break;
     case '/about':
-      res.writeHead(200, {'content-type':'text/plain' });
-      res.end('About');
+      serveStatic(res, '/static/about.html', 'text/plain');
       break;
+    case '/img/logo.jpg':
+      serveStatic(res, '/static/img/logo.jpg', 'image/jpeg');
     default:
-      res.writeHead(404, {'content-type':'text/plain' });
-      res.end('Not Found');
+      serveStatic(res, '/static/404.html', 'text/plain');
       break;
   }
 }).listen(3000);
