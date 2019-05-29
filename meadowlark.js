7b9d5d64159f967b010ddd15279976203c0d2dcd
Christopher Wozniak <cwozniak@aboutstaffing.com>
2019-05-29 13:10:00 -0600

    added jshint linter

--- meadowlark.js
+++ meadowlark.js
@@ -1,31 +1,29 @@
 const express = require('express');
 
 const app = express();
-
+const handlebars = require('express-handlebars').create({defaultLayout:'main'});
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
 app.set('port', process.env.PORT || 3000);
 
 app.get('/', (req, res) => {
-  res.type('text/plain');
-  res.send('Meadowlark Travel');
+  res.render('home');
 });
 
 app.get('/about', (req, res) => {
-  res.type('text/plain');
-  res.send('About Meadowlark Travel');
+  res.render('about');
 });
 
 //Custom 404 page
-app.use((err, req, res, next) => {
-  res.type('text/plain');
+app.use((err, res, next) => {
   res.status(404);
-  res.send('404 - not found');
+  res.render('404');
 });
 
 //Custom 500 page
 app.use((err, req, res, next) => {
-  res.type('text/plain');
   res.status(500);
-  res.send('500 - server error');
+  res.render('500');
 });
 
 app.listen(app.get('port'), () => {
